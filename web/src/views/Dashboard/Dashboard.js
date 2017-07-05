620af4949cd6b219b908169e3d6e574531089917
Peter Kneale <peterkneale@gmail.com>
2017-07-05 23:09:39 +1000

    add web

---
+++ web/src/views/Dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        Hello World
+        <div className="card card-inverse card-info">
+          <div className="card-header">Card title</div>
+          <div className="card-block">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </div>
+        </div>
+        <div className="card card-inverse card-info">
+          <div className="card-header">Card title</div>
+          <div className="card-block">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </div>
+        </div>
+        <div className="card card-inverse card-info">
+          <div className="card-header">Card title</div>
+          <div className="card-block">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </div>
+        </div>
+      </div>
+      
+    )
+  }
+}
+
+export default Dashboard;
