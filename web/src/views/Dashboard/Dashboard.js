6b8e7c24a03d2327714bdcbc0ae1882f42c6ffe4
Peter Kneale <peterkneale@gmail.com>
2017-07-14 23:19:47 +1000

    change dashboard

--- web/src/views/Dashboard/Dashboard.js
+++ web/src/views/Dashboard/Dashboard.js
@@ -1,27 +1,75 @@
 import React, { Component } from 'react';
 
-class Dashboard extends Component {
-
+class Cards extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        Hello World
-        <div className="card card-inverse card-info">
-          <div className="card-header">Card title</div>
-          <div className="card-block">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </div>
-        </div>
-        <div className="card card-inverse card-info">
-          <div className="card-header">Card title</div>
-          <div className="card-block">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </div>
-        </div>
-        <div className="card card-inverse card-info">
-          <div className="card-header">Card title</div>
-          <div className="card-block">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </div>
+        <div className="row">
+          <div className="col-sm-6 col-md-4">
+            <div className="card card-accent-primary">
+              <div className="card-header">
+                Card with accent
+              </div>
+              <div className="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-md-4">
+            <div className="card card-accent-secondary">
+              <div className="card-header">
+                Card with accent
+              </div>
+              <div className="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-md-4">
+            <div className="card card-accent-success">
+              <div className="card-header">
+                Card with accent
+              </div>
+              <div className="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-md-4">
+            <div className="card card-accent-info">
+              <div className="card-header">
+                Card with accent
+              </div>
+              <div className="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-md-4">
+            <div className="card card-accent-warning">
+              <div className="card-header">
+                Card with accent
+              </div>
+              <div className="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-md-4">
+            <div className="card card-accent-danger">
+              <div className="card-header">
+                Card with accent
+              </div>
+              <div className="card-block">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      
+
     )
   }
 }
 
-export default Dashboard;
+export default Cards;
